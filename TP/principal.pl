41cab28768c67ee9b461140c8e264ea3f3c5c1b4
TobySeirgalea <tobyseirgalea@gmail.com>
2024-11-15 20:52:09 -0300

    2 versiones de realizar lecturas

--- TP/principal.pl
+++ TP/principal.pl
@@ -29,7 +29,7 @@ buffersUsados(paralelo(Proceso1, Proceso2), L)  :- buffersUsados(Proceso1, Buffe
 sinRepetidos([]).
 sinRepetidos([X|XS]) :- not(member(X, XS)), sinRepetidos(XS).
 
-%%ordenCreciente(+Lista)
+%%ordenCreciente(+Lista) 
 ordenCreciente([]).
 ordenCreciente([_]).
 ordenCreciente([X, Y|YS]) :- X =< Y, ordenCreciente([Y|YS]). 
@@ -65,69 +65,64 @@ serializar(paralelo(P, Q), L)  :- serializar(P, L1),
 %% contenidoBuffer(+Buffer,+ProcesoOLista,?Contenidos)
 contenidoBuffer(_, [], []).
 contenidoBuffer(Buffer, Serializacion, ContenidosBuffer) :- Serializacion = [_|_], %Se podría utilizar is_list también
-                                                            realizarLecturas(Buffer, Serializacion, Contenidos),
-                                                            noLeoNadaNoEscritoPreviamente(Contenidos),
-                                                            soloContenidoDel(Buffer, Contenidos, ContenidosBuffer).
+                                                            realizarLecturas(Buffer, Serializacion, BufferConLecturasRealizadas),
+                                                            noLeoNadaNoEscritoPreviamente(BufferConLecturasRealizadas),
+                                                            soloContenidoDel(Buffer, BufferConLecturasRealizadas, ContenidosBuffer).
 contenidoBuffer(Buffer, Proceso, ContenidosBuffer)       :- serializar(Proceso, Serializacion), 
-                                                            realizarLecturas(Buffer, Serializacion, Contenidos), 
-                                                            noLeoNadaNoEscritoPreviamente(Contenidos),
-                                                            soloContenidoDel(Buffer, Contenidos, ContenidosBuffer).
+                                                            realizarLecturas(Buffer, Serializacion, BufferConLecturasRealizadas), 
+                                                            noLeoNadaNoEscritoPreviamente(BufferConLecturasRealizadas),
+                                                            soloContenidoDel(Buffer, BufferConLecturasRealizadas, ContenidosBuffer).
 
-%%noLeoNadaNoEscritoPreviamente(+Serialización)
+%% noLeoNadaNoEscritoPreviamente(+Serialización)
 noLeoNadaNoEscritoPreviamente(Serializacion) :- reverse(Serializacion, SerializacionEnOrdenLectura),
                                                 lecturasLuegoDeEscrituras(SerializacionEnOrdenLectura).
 
-
-
+%% soloContenidoDel(+Buffer, +ListaProcesos, -ListaContenidosBuffer) : Filtra y instancia en ListaContenidosBuffer la lista con sólo los contenidos del Buffer
 soloContenidoDel(Buffer, [], []).
-soloContenidoDel(Buffer, [escribir(Buffer, Contenido)|XS], [Contenido|L]) :- soloContenidoDel(Buffer, XS, L). 
-soloContenidoDel(Buffer, [X|XS], L)                                       :- X \= escribir(Buffer, _), 
-                                                                             soloContenidoDel(Buffer, XS, L).
+soloContenidoDel(Buffer, [escribir(Buffer, Contenido)|RestoProcesos], [Contenido|RestoContenidoBuffer]) :- soloContenidoDel(Buffer, RestoProcesos, RestoContenidoBuffer). 
+soloContenidoDel(Buffer, [ProcesoActual|RestoProcesos], RestoContenidoBuffer)                           :- ProcesoActual \= escribir(Buffer, _), 
+                                                                                                           soloContenidoDel(Buffer, RestoProcesos, RestoContenidoBuffer).
 
-%%lecturasLuegoDeEscrituras(+Serialización)
+%% lecturasLuegoDeEscrituras(+Serialización)
 lecturasLuegoDeEscrituras([]).
 lecturasLuegoDeEscrituras([Proceso|Serializacion])      :- Proceso \= leer(_),
                                                            lecturasLuegoDeEscrituras(Serializacion).
-lecturasLuegoDeEscrituras([leer(Buffer)|Serializacion]) :- memberchk(escribir(Buffer, _), Serializacion), siPerteneceLoSaco(escribir(Buffer, _), Serializacion, L), lecturasLuegoDeEscrituras(L).
+lecturasLuegoDeEscrituras([leer(Buffer)|Serializacion]) :- memberchk(escribir(Buffer, _), Serializacion), 
+                                                           siPerteneceLoSaco(escribir(Buffer, _), Serializacion, L), 
+                                                           lecturasLuegoDeEscrituras(L).
 
-%%realizarLecturas(+Buffer, +Lista, -ListaConLecturasRealizadas) : Realiza las lecturas del buffer, instancia en ListaConLecturasRealizadas el buffer resultante serializado
-realizarLecturas(Buffer, [], [], []).
-realizarLecturas(Buffer, [escribir(Buffer, Contenido)|XS], L) :- memberchk(leer(Buffer), XS), 
-                                                                                             siPerteneceLoSaco(leer(Buffer), XS, R),
-                                                                                             realizarLecturas(Buffer, R, L).
-realizarLecturas(Buffer, [escribir(Buffer, Contenido)|XS], [escribir(Buffer, Contenido)|XS]) :- not(memberchk(leer(Buffer), XS)).
-realizarLecturas(Buffer, [Proceso|XS], [Proceso|L])                                          :- Proceso \= escribir(Buffer,_),
-                                                                                                                realizarLecturas(Buffer, XS, L).
+%% realizarLecturas(+Buffer, +Lista, -ListaConLecturasRealizadas) : Realiza las lecturas del buffer, instancia en ListaConLecturasRealizadas el buffer resultante serializado
+% realizarLecturas(Buffer, [], [], []).
+% realizarLecturas(Buffer, [escribir(Buffer, Contenido)|ListaProcesos], ListaConLecturasRealizadas)                  :- memberchk(leer(Buffer), ListaProcesos), 
+%                                                                                                                       siPerteneceLoSaco(leer(Buffer), ListaProcesos, LecturaRealizada),
+%                                                                                                                       realizarLecturas(Buffer, LecturaRealizada, ListaConLecturasRealizadas).
+% realizarLecturas(Buffer, [escribir(Buffer, Contenido)|ListaProcesos], [escribir(Buffer, Contenido)|ListaProcesos]) :- not(memberchk(leer(Buffer), ListaProcesos)).
+% realizarLecturas(Buffer, [Proceso|ListaProcesos], [Proceso|ListaConLecturasRealizadas])                            :- Proceso \= escribir(Buffer,_),
+%                                                                                                                       realizarLecturas(Buffer, ListaProcesos, ListaConLecturasRealizadas).
  
-
-
-
-
-%%siPerteneceLoSaco(+P, +Lista, ?XS) : Si P pertenece a Lista instancia en XS la Lista-{primera aparición de P} sino falla.
+%% siPerteneceLoSaco(+P, +Lista, ?XS) : Si P pertenece a Lista instancia en XS la Lista-{primera aparición de P} sino falla.
 siPerteneceLoSaco(Proceso, [], []).
 siPerteneceLoSaco(Proceso, [Proceso|ListaProcesos], ListaProcesos).
 siPerteneceLoSaco(Proceso, [Proceso1|ListaProcesos], [Proceso1|ListaProcesosFinal]) :- Proceso1 \= Proceso, 
                                                                                        siPerteneceLoSaco(Proceso, ListaProcesos, ListaProcesosFinal).
 
-
 %% Ejercicio 6
 %% contenidoLeido(+ProcesoOLista,?Contenidos)
 contenidoLeido(Serializacion, Contenido) :- Serializacion = [_|_], 
-                                   noLeoNadaNoEscritoPreviamente(Serializacion),
-                                   realizarLecturas(Serializacion, Contenido).
-contenidoLeido(Procesos, Contenido) :- serializar(Procesos, Serializacion), 
-                                   noLeoNadaNoEscritoPreviamente(Serializacion),
-                                   realizarLecturas(Serializacion, Contenido).
+                                            noLeoNadaNoEscritoPreviamente(Serializacion),
+                                            realizarLecturas(Serializacion, Contenido).
+contenidoLeido(Procesos, Contenido)      :- serializar(Procesos, Serializacion), 
+                                            noLeoNadaNoEscritoPreviamente(Serializacion),
+                                            realizarLecturas(Serializacion, Contenido).
 
-
-                                   
-realizarLecturas([], []).
-realizarLecturas([escribir(Buffer, Contenido)|XS], [Contenido|L]) :- memberchk(leer(Buffer), XS), 
-                                                                     siPerteneceLoSaco(leer(Buffer), XS, R),
-                                                                     realizarLecturas(R, L).
-realizarLecturas([escribir(Buffer, Contenido)|XS], L)             :- not(memberchk(leer(Buffer), XS)), realizarLecturas(XS, L).
-realizarLecturas([Proceso|XS], L)                       :- Proceso \= escribir(_, _),
-                                                                     realizarLecturas(XS, L).
+%% realizarLecturas(+ListaProcesos, -ListaContenidosLeidos, -ListaConLecturasRealizadas)
+realizarLecturas([], [], []).
+realizarLecturas([escribir(Buffer, Contenido)|RestoProcesos], [Contenido|ListaContenidosLeidos], ListaConLecturasRealizadas)                    :- memberchk(leer(Buffer), RestoProcesos), 
+                                                                                                                                                   siPerteneceLoSaco(leer(Buffer), RestoProcesos, ProcesosTrasLectura),
+                                                                                                                                                   realizarLecturas(ProcesosTrasLectura, ListaContenidosLeidos, ListaConLecturasRealizadas).
+realizarLecturas([escribir(Buffer, Contenido)|RestoProcesos], ListaContenidosLeidos, [escribir(Buffer, Contenido)|ListaConLecturasRealizadas])  :- not(memberchk(leer(Buffer), RestoProcesos)), realizarLecturas(RestoProcesos, ListaContenidosLeidos, ListaConLecturasRealizadas).
+realizarLecturas([Proceso|RestoProcesos], ListaContenidosLeidos, [Proceso|ListaConLecturasRealizadas])                                          :- Proceso \= escribir(_, _),
+                                                                                                                                                   realizarLecturas(RestoProcesos, ListaContenidosLeidos, ListaConLecturasRealizadas).
 
 
 
@@ -140,48 +135,51 @@ realizarLecturas([Proceso|XS], L)                       :- Proceso \= escribir(_
 esSeguro([]).
 esSeguro(Serializacion) :- Serializacion = [_|_],
                            noLeoNadaNoEscritoPreviamente(Serializacion).
-esSeguro(Procesos) :-     procesosParalelosNoCompartenBuffer(Procesos), 
-                          not((serializar(Procesos, Serializacion),
-                          not(esSeguro(Serializacion)))).
+esSeguro(Procesos)      :- procesosParalelosNoCompartenBuffer(Procesos), 
+                           not((serializar(Procesos, Serializacion),
+                           not(esSeguro(Serializacion)))).
 
+%% procesosParalelosNoCompartenBuffer(+Proceso)
 procesosParalelosNoCompartenBuffer(escribir(_,_)).
 procesosParalelosNoCompartenBuffer(computar).
 procesosParalelosNoCompartenBuffer(leer(_)).
-procesosParalelosNoCompartenBuffer(secuencia(P, Q)) :- procesosParalelosNoCompartenBuffer(P), procesosParalelosNoCompartenBuffer(Q). 
-procesosParalelosNoCompartenBuffer(paralelo(P,Q)) :- serializar(P, L1), serializar(Q, L2), noCompartenBuffers(L1, L2).
+procesosParalelosNoCompartenBuffer(secuencia(P, Q)) :- procesosParalelosNoCompartenBuffer(P),
+                                                       procesosParalelosNoCompartenBuffer(Q). 
+procesosParalelosNoCompartenBuffer(paralelo(P,Q))   :- serializar(P, SerializacionDeP), 
+                                                       serializar(Q, SerializacionDeQ), 
+                                                       noCompartenBuffers(SerializacionDeP, SerializacionDeQ).
 
-noCompartenBuffers([], L).
-noCompartenBuffers([escribir(Buffer, _)|PS], L2) :- not(memberchk(escribir(Buffer, _), L2)), noCompartenBuffers(PS, L2). 
-noCompartenBuffers([leer(Buffer)|PS], L2) :- not(memberchk(leer(Buffer))), noCompartenBuffers(PS, L2). 
+%% noComparteBuffers(+PrimerListaProcesos, +SegundaListaProcesos)
+noCompartenBuffers([], SegundaListaProcesos).
+noCompartenBuffers([escribir(Buffer, _)|RestoProcesos], SegundaListaProcesos) :- not(memberchk(escribir(Buffer, _), SegundaListaProcesos)), noCompartenBuffers(RestoProcesos, SegundaListaProcesos). 
+noCompartenBuffers([leer(Buffer)|RestoProcesos], SegundaListaProcesos)        :- not(memberchk(leer(Buffer))), noCompartenBuffers(RestoProcesos, SegundaListaProcesos). 
 
 %% Ejercicio 8
 %% ejecucionSegura( XS,+BS,+CS) - COMPLETAR LA INSTANCIACIÓN DE XS
-ejecucionSegura(XS, ListaBuffers, Contenidos) :-  desde(0, N), between(0, N, N2), generarTodasLasEjecucionesDe(ListaBuffers, Contenidos, XS, N), length(XS, N2), esSeguro(XS).
+ejecucionSegura(Ejecucion, ListaBuffers, Contenidos) :- desde(0, N),
+                                                        between(0, N, Longitud),
+                                                        generarTodasLasEjecucionesDe(ListaBuffers, Contenidos, Ejecucion, N),
+                                                        length(Ejecucion, Longitud), 
+                                                        esSeguro(Ejecucion).
 
-%generar unidireccional y llamar con permutaciones de bs y cs
+%%
+generarTodasLasEjecucionesDe(_, _, [], 0).
+generarTodasLasEjecucionesDe(ListaBuffers, ListaContenidos, [escribir(Buffer, Contenido)|RestoProcesos], N) :- N >= 0, 
+                                                                                                               member(Buffer, ListaBuffers),
+                                                                                                               member(Contenido, ListaContenidos), 
+                                                                                                               N2 is N-1,
+                                                                                                               generarTodasLasEjecucionesDe(ListaBuffers, ListaContenidos, RestoProcesos, N2). 
+generarTodasLasEjecucionesDe(ListaBuffers, ListaContenidos, [leer(Buffer)|RestoProcesos], N)                :- N >= 0,
+                                                                                                               member(Buffer, ListaBuffers),
+                                                                                                               N2 is N-1,
+                                                                                                               generarTodasLasEjecucionesDe(ListaBuffers, ListaContenidos, RestoProcesos, N2). 
+generarTodasLasEjecucionesDe(ListaBuffers, ListaContenidos, [computar|RestoProcesos], N)                    :- N >= 0,
+                                                                                                               N2 is N-1,
+                                                                                                               generarTodasLasEjecucionesDe(ListaBuffers, ListaContenidos, RestoProcesos, N2).
 
-generarTodasLasEjecucionesDe(_,_,[],0).
-generarTodasLasEjecucionesDe(BS, CS, [computar|XS] , N) :- N > 0, N2 is N - 1, generarTodasLasEjecucionesDe([B1|BS], [C1|CS], XS, N2).
-generarTodasLasEjecucionesDe(BS, CS, [escribir(B, C)|XS] , N) :- N > 0, N2 is N - 1, member(BS, B), member(CS, C),generarTodasLasEjecucionesDe(BS, CS, XS, N2).
-generarTodasLasEjecucionesDe(BS, CS, [leer(B)|XS] , N) :- N > 0, N2 is N - 1, member(BS, B),generarTodasLasEjecucionesDe(BS, CS, XS, N2).
-
-permutaciones([], []).
-permutaciones([X|XS], L) :- var(L), permutaciones(XS, L1), agregarEnTodasPartes(X, L1, L).
-%permutacion(+L1, +L2) sería más eficiente si implementamos una función 'mismosElementos' / que si L1 y L2 tienen los mismos elementos y longitud, entonces una es permutación de la otra
-permutaciones([X|XS], L) :- nonvar(L), length([X|XS], L1), length(L, L1), mismosElementos([X|XS], L).
-
+%%
 desde(X,X).
 desde(X, Y) :- X1 is X+1, desde(X1, Y).
-
-%mismosElementos(+L1, +L2)
-mismosElementos([], []).
-mismosElementos(L1, L2) :- pertenece(X, L1), pertenece(X, L2). 
-
-%agregarEnTodasPartes(+E, +L, -LS)
-agregarEnTodasPartes(E, [], [E]).
-agregarEnTodasPartes(E, [X|XS], [E,X|XS]).
-agregarEnTodasPartes(E, [X|XS], [X|L]) :- agregarEnTodasPartes(E, XS, L).
-
 
 
   %% 8.1. Analizar la reversibilidad de XS, justificando adecuadamente por qué el predicado se comporta como
